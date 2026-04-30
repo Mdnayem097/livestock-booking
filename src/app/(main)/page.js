f125ca42606c85dd56b81191d9e922269fc8ed15
Mdnayem097 <mdneloy097@gmail.com>
2026-05-01 01:36:32 +0600

    I decorated the animals with price tags.

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -25,38 +25,43 @@ export default async function Home() {
 
       <div>
         <section className="max-w-7xl mx-auto px-5 py-10">
-
           <h2 className="text-2xl font-bold mb-6">
             Featured Animals
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-            {data.map((animal) => (
-              <Link key={animal.id} href={`/animals/${animal.id}`}>
-                <div className="border border-green-600 rounded-lg p-3 hover:shadow cursor-pointer">
+            {[...data]
+              .sort((a, b) => a.price - b.price)
+              .map((animal) => (
 
-                  <div className="relative w-full h-[180px]">
-                    <Image
-                      src={animal.image}
-                      alt={animal.name}
-                      fill
-                      className="object-cover rounded"
-                    />
+                <Link key={animal.id} href={`/animals/${animal.id}`}>
+
+                  <div className="border border-green-600 rounded-lg p-3 hover:shadow-lg transition cursor-pointer">
+
+                    <div className="relative w-full h-[180px]">
+                      <Image
+                        src={animal.image}
+                        alt={animal.name}
+                        fill
+                        className="object-cover rounded"
+                      />
+                    </div>
+                    <h3 className="mt-2 font-semibold">
+                      {animal.name}
+                    </h3>
+                    <p className="text-green-600 font-medium">
+                      ৳{animal.price}
+                    </p>
+                    <p className="text-sm text-gray-500">
+                      {animal.location}
+                    </p>
+
                   </div>
 
-                  <h3 className="mt-2 font-semibold">{animal.name}</h3>
+                </Link>
 
-                  <p className="text-green-600 font-medium">
-                    ৳{animal.price}
-                  </p>
+              ))}
 
-                  <p className="text-sm text-gray-500">
-                    {animal.location}
-                  </p>
-
-                </div>
-              </Link>
-            ))}
           </div>
         </section>
 
